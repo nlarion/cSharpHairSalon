e3d65258080de4db17194d3e8377cb7cd6dc467b
Neil Larion <neil.larion@gmail.com>
2016-02-26 10:00:25 -0800

    add Find method to Stylist class

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -100,6 +100,39 @@ namespace HairSalon
       }
       return myListStylist;
     }
+    public static Stylist Find(int id)
+    {
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM stylists WHERE id = @StylistId;", conn);
+      SqlParameter StylistIdParameter = new SqlParameter();
+      StylistIdParameter.ParameterName = "@StylistId";
+      StylistIdParameter.Value = id.ToString();
+      cmd.Parameters.Add(StylistIdParameter);
+      rdr = cmd.ExecuteReader();
+
+      int foundStylistId = 0;
+      string foundStylistName = null;
+
+      while(rdr.Read())
+      {
+        foundStylistId = rdr.GetInt32(0);
+        foundStylistName = rdr.GetString(1);
+      }
+      Stylist foundStylist = new Stylist(foundStylistName, foundStylistId);
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return foundStylist;
+    }
     public static void DeleteAll()
     {
       SqlConnection conn = DB.Connection();
