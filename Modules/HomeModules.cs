90a3469719d3b6ce0112e20cb99da84c0c4f645d
Neil Larion <neil.larion@gmail.com>
2016-02-26 11:04:51 -0800

    Add Stylist pages view/create to the views folder and create pointers in HomeModules.Cs

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -11,9 +11,39 @@ namespace HairSalon
     {
       Get["/"] = _ => {
         return View["index.cshtml"];
-      };      
+      };
       Get["/Stylist"] = _ => {
         return View["stylist.cshtml",Stylist.GetAll()];
+      };
+      Post["/Stylist"] = _ => {
+        Stylist newStylist = new Stylist(Request.Form["name"]);
+        newStylist.Save();
+        return View["stylist.cshtml", Stylist.GetAll()];
+      };
+      Get["/Stylist/{id}"]  = parameters => {
+        Stylist newStylist = Stylist.Find(parameters.id);
+        //List<Client> clientList = Client.FindByStylistId(newStylist.GetId());
+        Dictionary<string,object> myDictionary = new Dictionary<string,object>{};
+        myDictionary.Add("stylist",newStylist);
+        //myDictionary.Add("clients",clientList);
+        return View["StylistView.cshtml",myDictionary];
+      };
+      Post["/Stylist/Update/{id}"]  = parameters => {
+        Stylist newStylist = Stylist.Find(parameters.id);
+        newStylist.Update(Request.Form["name"]);
+        return View["stylist.cshtml",Stylist.GetAll()];
+      };
+      Get["/Stylist/Delete/{id}"]  = parameters => {
+        Stylist newStylist = Stylist.Find(parameters.id);
+        newStylist.Delete();
+        return View["stylist.cshtml",Stylist.GetAll()];
+      };
+      Get["/Stylist/Create"]  = _ => {
+        return View["StylistCreate.cshtml"];
+      };
+      Get["/Stylist/Delete"] = _ => {
+        Stylist.DeleteAll();
+        return View["stylist.cshtml",  "delete"];
       };
     }
   }
